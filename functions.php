62a97cddfb428a27d3bf3103b4e8685f8819a3d0
s-tani09 <s.tanigami09@gmail.com>
2023-09-23 04:19:07 +0900

    修正

--- functions.php
+++ functions.php
@@ -294,29 +294,28 @@ add_filter( 'wpcf7_form_tag', 'dynamic_field_values', 10, 2);
 
 // 記事のPVを取得
 function getPostViews($postID) {
-  $count_key = 'post_views_count';
-  $count = get_post_meta($postID, $count_key, true);
-  if ($count=='') {
-    delete_post_meta($postID, $count_key);
-    add_post_meta($postID, $count_key, '0');
-    return "0 View";
-  }
-  return $count.' Views';
+	$count_key = 'post_views_count';
+	$count = get_post_meta($postID, $count_key, true);
+	if ($count=='') {
+		delete_post_meta($postID, $count_key);
+		add_post_meta($postID, $count_key, '0');
+		return "0 View";
+	}
+	return $count.' Views';
 }
 
 // 記事のPVをカウントする
 function setPostViews($postID) {
-  $count_key = 'post_views_count';
-  $count = get_post_meta($postID, $count_key, true);
-  if ($count=='') {
-    $count = 0;
-    delete_post_meta($postID, $count_key);
-    add_post_meta($postID, $count_key, '0');
-  } else {
-    $count++;
-    update_post_meta($postID, $count_key, $count);
-  }
-
+	$count_key = 'post_views_count';
+	$count = get_post_meta($postID, $count_key, true);
+	if ($count=='') {
+		$count = 0;
+		delete_post_meta($postID, $count_key);
+		add_post_meta($postID, $count_key, '0');
+	} else {
+		$count++;
+		update_post_meta($postID, $count_key, $count);
+	}
 }
 remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 
